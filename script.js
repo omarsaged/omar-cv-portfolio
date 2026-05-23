d0483d7e1f0479e271246827a7e5d9c36bf848de
omarsaged <omarsagedhindawy26@gmail.com>
2026-05-23 23:04:30 +0200

    Updated CV page and bash screenshot

--- script.js
+++ script.js
@@ -38,3 +38,52 @@ if (themeButton) {
     });
 
 }
+function showAbout() {
+    let about = document.getElementById("aboutText");
+
+    if (about.style.display === "none") {
+        about.style.display = "block";
+    } else {
+        about.style.display = "none";
+    }
+}
+
+function showEducation() {
+    let education = document.getElementById("educationList");
+
+    if (education.style.display === "none") {
+        education.style.display = "block";
+    } else {
+        education.style.display = "none";
+    }
+}
+
+function showSkills() {
+    let skills = document.getElementById("skillsList");
+
+    if (skills.style.display === "none") {
+        skills.style.display = "block";
+    } else {
+        skills.style.display = "none";
+    }
+}
+
+function showExperience() {
+    let experience = document.getElementById("experienceText");
+
+    if (experience.style.display === "none") {
+        experience.style.display = "block";
+    } else {
+        experience.style.display = "none";
+    }
+}
+
+function showLanguages() {
+    let languages = document.getElementById("languagesList");
+
+    if (languages.style.display === "none") {
+        languages.style.display = "block";
+    } else {
+        languages.style.display = "none";
+    }
+}
